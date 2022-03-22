173a4eedf934fbea4da0340a42d390dfea74c6d9
Jacob Molsby <jacobmolsby@hotmail.se>
2022-03-22 18:26:45 +0000

    Added BlogUser model to users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User 
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class BlogUser(models.Model):
+    """
+    A model for the user to update profile information.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=254, null=True, blank=True)
+    last_name = models.CharField(max_length=254, null=True, blank=True)
+    email = models.EmailField()
+    bio = models.TextField()
+    profile_image = CloudinaryField('image', default='placeholder')
+    joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name + self.last_name
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def profile(request):
+    """
+    A view to render the profile of the user
+    """
+
+    template_name = "users/profile.html"
+    context = {}
+
+    return render(request, template_name, context)
