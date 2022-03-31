ed304f257013e5db7de84cfc59293e469b046dcc
Jacob <jacobmolsby@hotmail.se>
2022-03-31 19:14:01 +0200

    Deployment part 2 -- Staticfiles change, settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = [
     'localhost',
