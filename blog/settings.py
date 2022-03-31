db29495cb54c3e759df57f0b36df77afd0e97a2b
Jacob <jacobmolsby@hotmail.se>
2022-03-31 20:35:51 +0200

    Removed media_root from settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -139,7 +139,7 @@ else:
         }
     }
 
-
+CloudinaryField('image', default='p
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -179,15 +179,13 @@ USE_TZ = True
 
 CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 
-MEDIA_URL = '/media/'
-MEDIAFILES_DIRS = (os.path.join(BASE_DIR, 'media'),)
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
