1de48a404a34921644cd0462de10cc4d16fab3fc
Jacob <jacobmolsby@hotmail.se>
2022-04-03 20:19:02 +0200

    Comment out tests

--- posts/tests/test_models.py
+++ posts/tests/test_models.py
@@ -1,29 +1,29 @@
-from django.test import TestCase, Client
-from django.urls import reverse
-from posts.models import Post, Comment
-from users.models import BlogUser
+# from django.test import TestCase, Client
+# from django.urls import reverse
+# from posts.models import Post, Comment
+# from users.models import BlogUser
 
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
-import datetime
+# import datetime
 
-class TestModels(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.add_post = reverse('profile')
-        self.like_post = reverse('like', args=['some-title'])
-        self.user = User.objects.create_user(username='testuser', password='12345')
-        self.user2 = User.objects.create_user(username='undefined', password='undefined')
-        login = self.client.login(username='testuser', password='12345')
-        self.blog_user = BlogUser.objects.get(user=self.user)
-        self.post1 = Post.objects.create(
-            author=self.blog_user,
-            post_image=None,
-            post_title="Some Random Title",
-            post_body='some random content',
-            posted=datetime.datetime,
-            status=1,
-        )
+# class TestModels(TestCase):
+#     def setUp(self):
+#         self.client = Client()
+#         self.add_post = reverse('profile')
+#         self.like_post = reverse('like', args=['some-title'])
+#         self.user = User.objects.create_user(username='testuser', password='12345')
+#         self.user2 = User.objects.create_user(username='undefined', password='undefined')
+#         login = self.client.login(username='testuser', password='12345')
+#         self.blog_user = BlogUser.objects.get(user=self.user)
+#         self.post1 = Post.objects.create(
+#             author=self.blog_user,
+#             post_image=None,
+#             post_title="Some Random Title",
+#             post_body='some random content',
+#             posted=datetime.datetime,
+#             status=1,
+#         )
     
-    def test_post_slug_is_slug_on_creation(self):
-        self.assertEquals(self.post1.slug, 'some-random-title')
+#     def test_post_slug_is_slug_on_creation(self):
+#         self.assertEquals(self.post1.slug, 'some-random-title')
--- posts/tests/test_urls.py
+++ posts/tests/test_urls.py
@@ -1,60 +1,60 @@
-from django.test import TestCase
-from django.urls import reverse, resolve
-from posts.views import (
-    edit_post, 
-    delete_post, 
-    add_post,
-    post_detail, 
-    like, 
-    delete_comment
-)
-from users.views import (
-    profile,
-    edit_profile
-)
+# from django.test import TestCase
+# from django.urls import reverse, resolve
+# from posts.views import (
+#     edit_post, 
+#     delete_post, 
+#     add_post,
+#     post_detail, 
+#     like, 
+#     delete_comment
+# )
+# from users.views import (
+#     profile,
+#     edit_profile
+# )
 
 
-class TestUrls(TestCase):
-    # add_post
-    def test_post_urls_resolved(self):
-        url = reverse('add_post')
-        print('Add post URL -- PASSED')
-        self.assertEquals(resolve(url).func, add_post)
+# class TestUrls(TestCase):
+#     # add_post
+#     def test_post_urls_resolved(self):
+#         url = reverse('add_post')
+#         print('Add post URL -- PASSED')
+#         self.assertEquals(resolve(url).func, add_post)
 
-    # edit_post
-    def test_post_urls_resolved2(self):
-        url = reverse('edit_post', args=['slug'])
-        print('Edit post URL -- PASSED')
-        self.assertEquals(resolve(url).func, edit_post)
+#     # edit_post
+#     def test_post_urls_resolved2(self):
+#         url = reverse('edit_post', args=['slug'])
+#         print('Edit post URL -- PASSED')
+#         self.assertEquals(resolve(url).func, edit_post)
 
-    # delete_post
-    def test_post_urls_resolved3(self):
-        url = reverse('delete', args=['slug'])
-        print("Delete post URL -- PASSED")
-        self.assertEquals(resolve(url).func, delete_post)
+#     # delete_post
+#     def test_post_urls_resolved3(self):
+#         url = reverse('delete', args=['slug'])
+#         print("Delete post URL -- PASSED")
+#         self.assertEquals(resolve(url).func, delete_post)
 
-    # post_detail
-    def test_post_urls_resolved4(self):
-        url = reverse('post_detail', args=['slug'])
-        print('post_detail URL -- PASSED')
-        self.assertEquals(resolve(url).func, post_detail)
+#     # post_detail
+#     def test_post_urls_resolved4(self):
+#         url = reverse('post_detail', args=['slug'])
+#         print('post_detail URL -- PASSED')
+#         self.assertEquals(resolve(url).func, post_detail)
  
 
-    # like
-    def test_post_urls_resolved5(self):
-        url = reverse('like', args=['slug'])
-        print('Like post URL -- PASSED')
-        self.assertEquals(resolve(url).func, like)
+#     # like
+#     def test_post_urls_resolved5(self):
+#         url = reverse('like', args=['slug'])
+#         print('Like post URL -- PASSED')
+#         self.assertEquals(resolve(url).func, like)
 
-    # Profile
-    def test_post_urls_resolved(self):
-        url = reverse('profile')
-        print('Profile URL -- PASSED')
-        self.assertEquals(resolve(url).func, profile)
+#     # Profile
+#     def test_post_urls_resolved(self):
+#         url = reverse('profile')
+#         print('Profile URL -- PASSED')
+#         self.assertEquals(resolve(url).func, profile)
 
-    # Edit Profile
-    def test_post_urls_resolved2(self):
-        url = reverse('edit')
-        print('Edit profile URL -- PASSED')
-        self.assertEquals(resolve(url).func, edit_profile)
+#     # Edit Profile
+#     def test_post_urls_resolved2(self):
+#         url = reverse('edit')
+#         print('Edit profile URL -- PASSED')
+#         self.assertEquals(resolve(url).func, edit_profile)
 
